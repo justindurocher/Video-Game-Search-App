81255bb3a4ee45de7f474f01cfdcab0a33554b8e
Nathan Sartain <nathanmsartain@gmail.com>
2020-01-09 21:04:28 -0500

    Pulled 3 random video games and displayed their thumbnails.

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$.ajax({
+  url: 'https://api.rawg.io/api/games',
+  method: 'GET'
+}).then(function(response) {
+  console.log(response);
+});
+
+
+// get 3 random games
+for (let i = 0; i < 3; i++) {
+  let num = Math.floor(Math.random() * 354144);
+  console.log(num);
+  queryRawg(num, i + 1);
+}
+
+function queryRawg(gameID, index) {
+  $.ajax({
+    url: 'https://api.rawg.io/api/games/' + gameID,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+    let elID = '#game-thumb-' + index;
+    console.log(elID);
+    $(elID).attr('src', response.background_image);
+  });
+}
+
