3321f1e6b251b730cceb826458085484b4496e48
Ouro <ourosalimabdul@gmail.com>
2020-01-14 21:11:49 -0500

    Need to add new classes to css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,57 @@ function queryRawg(gameID, index) {
     $(elID).attr('src', response.background_image);
   });
 }
+//////////
+//Ab
+////////////
+
+
+// var searchFromIndexPage = $(".search.link.icon").val();
+// var searchIconFromIndexPage = $("#searchIcon");
+var searchFromDisplayPage = $(".search.link.icon").val();
+var seacrIconFromDisplayPage = $(".search.link");
+
+// searchIconFromIndexPage.on("click", function(event)
+// {
+//     event.preventDefault();
+//     console.log("calling from Index")
+//     getInput(searchFromIndexPage);
+// });
+seacrIconFromDisplayPage.on("click", function(event)
+{
+    event.preventDefault();
+    console.log("calling from Display");
+    console.log(seacrIconFromDisplayPage);
+    getInput (searchFromDisplayPage);
+});
+
+function getInput(userInput){
+  
+  console.log("user search " + userInput);
+  //$(".searchData").text(yourSearch);
+  uSearch(userInput);
+}
+// var yourSeach = $(".ui.transparent.icon.input").text();
+// console.log("user search " + yourSeach);
+// $(".ui.transparent.icon.input")
+//   .search({
+//     type: "Title",
+//     source: response
+//   });
+
+  var queryURL = "https://api.rawg.io/api/games?page_size=5&search=";
+
+  function uSearch(title) {
+    $.ajax({
+      url: queryURL + title,
+      method: 'GET'
+    }).then(function(response) {
+      console.log(response);
+      var result = response.description;
+      console.log($(result));
+      
+    });
+  
+}
+
+
